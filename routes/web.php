c1982f0ccab77f1936d674d4d7d9940848bff4bb
giorgosmargaritis <gmargaritis17@gmail.com>
2023-09-06 20:12:08 +0300

    Basic Functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::all(),
+    ]);
 });
+
+Route::get('posts/{post}', function($slug) {
+    return view('post', [
+        'post' => Post::find($slug),
+    ]);
+});//->where('post', '[A-z_\+');
